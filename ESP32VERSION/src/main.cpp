164d79f5f5b7849e11cb27e9187043c11005fa4b
Zahar <z.zitsw@gmail.com>
2023-09-18 20:21:35 -0400

    changed structure of variables for encoder project esp32

--- ESP32VERSION/src/main.cpp
+++ ESP32VERSION/src/main.cpp
@@ -1,36 +1,70 @@
-const int CLOCK_PIN = 25;
-const int DATA_PIN = 26;
-const int BIT_COUNT = 24;
+const int CLOCK_PIN  {25};
+const int DATA_PIN  {26};
+const int BIT_COUNT  {24};
 
 
-const float formula2 = 0.0000153547881599881;
-const long formula1 = 8000000;
+const float formula2  {0.0000153547881599881};
+const long formula1  {8000000};
 
 
-unsigned long result2;
-float result3;
+unsigned long result2 {};
+float result3 {};
 
 
-unsigned long last_time;
-long displayDelay = 500;
+unsigned long last_time {};
+//long displayDelay  {500};
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 
-#define OLED_RESET     -1 
-#define SCREEN_ADDRESS 0x3C 
-Adafruit_SSD1306 display(OLED_RESET);
+///#define OLED_RESET     -1 
+//#define SCREEN_ADDRESS 0x3C 
+//Adafruit_SSD1306 display(OLED_RESET);
+
+unsigned long readPosition();
+unsigned long shiftIn(const int data_pin, const int clock_pin, const int bit_count);
+//void displayPrint(unsigned long displayValue);
+
+void setup() {
+  //setup our pins
+  pinMode(DATA_PIN, INPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+
+  //give some default values
+  digitalWrite(CLOCK_PIN, HIGH);
+
+  Wire.begin();
+  //display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); 
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  unsigned long reading = readPosition();
+  result2 = reading - formula1;
+  result3 = result2 * formula2;
+  Serial.print("$");
+  Serial.print(result3,4);
+  Serial.println(";");
+ // if (millis()- last_time > displayDelay){
+   // last_time = millis();
+   // displayPrint(result3);
+   // display.display();
+   // delay(500);
+ // }
+}
+
 
 //read the current angular position
-float readPosition() {
+unsigned long readPosition() {
   unsigned long sample1 = shiftIn(DATA_PIN, CLOCK_PIN, BIT_COUNT);
   return sample1;
 }
 
 //read in a byte of data from the digital input of the board.
-float shiftIn(const int data_pin, const int clock_pin, const int bit_count) {
+unsigned long shiftIn(const int data_pin, const int clock_pin, const int bit_count) {
   unsigned long data = 0;
   for (int i=0; i<bit_count; i++) {
     data <<= 1;
@@ -46,46 +80,21 @@ float shiftIn(const int data_pin, const int clock_pin, const int bit_count) {
   return data;
 }
 
-void displayPrint(unsigned long displayValue){
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0, 0);
-  display.print("Result:");
-  display.setTextSize(2);
-  display.setCursor(0, 8);
-  display.print(displayValue);
+// void displayPrint(unsigned long displayValue){
+//   display.clearDisplay();
+//   display.setTextColor(WHITE);
+//   display.setTextSize(1);
+//   display.setCursor(0, 0);
+//   display.print("Result:");
+//   display.setTextSize(2);
+//   display.setCursor(0, 8);
+//   display.print(displayValue);
 
-}
-
-void setup() {
-  //setup our pins
-  pinMode(DATA_PIN, INPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-
-  //give some default values
-  digitalWrite(CLOCK_PIN, HIGH);
-
-  Wire.begin();
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); 
-  Serial.begin(9600);
-
-}
+// }
 
 
-void loop() {
-  float reading = readPosition();
- // result2 = reading - formula1;
- // result3 = result2 * formula2;
-//  Serial.print("$");
-  Serial.println(reading,4);
-//  Serial.println(";");
-  if (millis()- last_time > displayDelay){
-    last_time = millis();
-   // displayPrint(result3);
-    display.display();
-   // delay(500);
-  }
-}
+
+
+
 
 
