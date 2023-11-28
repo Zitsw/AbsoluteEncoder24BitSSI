7dde3958ad8e91d7eb793734473d7ba8a89fea15
Zahar <z.zitsw@gmail.com>
2023-11-27 21:54:37 -0500

    Timeline

--- ESP32VERSION/src/main.cpp
+++ ESP32VERSION/src/main.cpp
@@ -4,15 +4,15 @@
 #include "FS.h"
 #include <SD.h>
 #include <RTClib.h>
-// #include <Adafruit_GFX.h>
-// #include <Adafruit_SSD1306.h>
+//#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <FreeRTOSConfig.h>
 //Pins
 const int CLOCK_PIN  {25};
 const int DATA_PIN  {26};
 const int BIT_COUNT  {24};
-const int SD_PIN {22};
-const int BUTTON_PIN {21};
+const int SD_PIN {14};
+const int BUTTON_PIN {33};
 const int LED_PIN {32};
 // Math
 const float formula2  {0.0000153547881599881};
@@ -24,25 +24,41 @@ int timing {12};
 int Btime {0};
 int timer_count[4] = {0, 0, 0, 0};
 unsigned long last_time {};
-long programDelay {20};
+long programDelay {};
+int convertYear {};
+int lastHour{};
+int lastMinute{};
 //Button
-boolean DynamicMode = false;
+boolean DynamicMode = 0;
 unsigned long lastDebounceTime {0};
 int buttonState;
 long DEBOUNCE_TIME {50};
 int lastButtonState = LOW;  // the previous reading from the input pin
-
+boolean dynamicTitle = true;
+boolean staticTitle = true;
 ///#define OLED_RESET     -1 
 //#define SCREEN_ADDRESS 0x3C 
 //Adafruit_SSD1306 display(OLED_RESET);
 //Initialization
-String titleDataMessage;
 String dataMessage;
+String title;
+String messageDIN;
+String timeDIN;
+//File Name
+String fileNameDAT;
+String fileNameDIN;
+String fileNameTimeDAT;
+String fileNameTimeDIN;
+
+
 File myFile;
 RTC_DS3231 rtc;
+//Defined functions
 void appendFile(fs::FS &fs, const char * path, const char * message);
+void createDir(fs::FS &fs, const char * path);
 unsigned long readPosition();
 unsigned long shiftIn(const int data_pin, const int clock_pin, const int bit_count);
+void createDirectories(const char* path);
 //void displayPrint(unsigned long displayValue);
 
 void setup() {
@@ -56,26 +72,32 @@ void setup() {
   digitalWrite(CLOCK_PIN, HIGH);
   Serial.begin(115200);
   Wire.begin();
-  
+  rtc.begin();
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  DateTime now = rtc.now();
+ 
   // SD card init
-  if(SD.begin(SD_PIN,SPI,40000000)){
-    Serial.print("SD is ready for use");
-  }else{
-    Serial.print("Check SD card connetion");
-  }
+  SD.begin(SD_PIN);
   createDir(SD, "/DAT");
-  //display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); 
-  titleDataMessage = "Время;Дата;ID Регистратора;Версия ПО;Канал;Тип\n10.02.2023;4:58:14;100;03.00;01;06\n";
-  appendFile(SD, "/mySCV.txt", titleDataMessage.c_str());
+  createDir(SD, "/DIN");
+  timeDIN = String(now.day()) + "." + String(now.month()) + "." + String(now.year()) + ";" + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()) ; 
+  convertYear = now.year() - 2000;
+  fileNameTimeDAT = String(now.hour()) + String(now.day())  + String(now.month()) + String(convertYear);
+  messageDIN = timeDIN + ";100;03.00;01;06" + "\n";
+  title = "Date;Time;ID;FW version;Channel;Mode;Value; \n";
+  fileNameDAT = "/DAT/" + fileNameTimeDAT + ".csv";
+  appendFile(SD,fileNameDAT.c_str(), title.c_str());
 }
 
 void loop() {
+  //time reading
+  DateTime now = rtc.now();
   //Button Reading
   int ButtonReading = digitalRead(BUTTON_PIN);
   if (ButtonReading != lastButtonState) {
     lastDebounceTime = millis();
   }
-    if ((millis() - lastDebounceTime) > DEBOUNCE_TIME) {
+   if ((millis() - lastDebounceTime) > DEBOUNCE_TIME) {
     if (ButtonReading != buttonState) {
       buttonState = ButtonReading;
       if (buttonState == HIGH) {
@@ -84,43 +106,55 @@ void loop() {
     }
   }
   lastButtonState = ButtonReading;
-//math
-  unsigned long reading = readPosition();
-  result2 = reading - formula1;
-  result3 = result2 * formula2;
-  // if (millis() - timing >= 1000){
-  //   timer_count[0]++; //sec
-  //   timing = millis();
-  // }
-  //   if (timer_count[0] == 60){ //min
-  //     timer_count[1]++;
-  //     timer_count[0] = 0;
-  //   }
-  //     if(timer_count[1] == 60){
-  //       timer_count[2]++;
-  //       timer_count[1] = 0;
-  //     }
-  //       if(timer_count[2] == 24){
-  //         timer_count[3]++;
-  //         timer_count[2] = 0;
-  //       }
-  dataMessage = String(result3, 6) + ";" + "\n";
+// //math
+   unsigned long reading = readPosition();
+   result2 = reading - formula1;
+   result3 = result2 * formula2;
 
-  if (DynamicMode == false){
-    programDelay = 5000;
-    digitalWrite(LED_PIN, LOW);
+//   dataMessage = String(result3, 6) + ";" + "\n"
+  int testHOUR = now.hour();
+  int testMIN = now.minute();
+ // Serial.println(testHOUR);
+  Serial.println(fileNameDAT);
+
+  
+  
+  if (DynamicMode == false){  
+  programDelay = 10000;
+  digitalWrite(LED_PIN, LOW);
+  dynamicTitle = true;
+  if(lastHour != now.hour()){
+    fileNameTimeDAT = String(now.hour()) + String(now.day())  + String(now.month()) + String(convertYear);
+    lastHour = now.hour();
+    fileNameDAT = "/DAT/" + fileNameTimeDAT + ".csv";
+    appendFile(SD, fileNameDAT.c_str(), title.c_str());
+  }
+  String time = String(now.day()) + "." + String(now.month()) + "." + String(now.year()) + ";" + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()) ;
+  dataMessage = time + ";100;03.00;01;V" + String(result3, 6) + ";" + "\n";
     if (millis()- last_time > programDelay){
     last_time = millis();
-    appendFile(SD, "/mySCV.txt", dataMessage.c_str());
+    Serial.println(time);
+    appendFile(SD, fileNameDAT.c_str(), dataMessage.c_str());
     }
   }
-
-  if (DynamicMode == true){
-    programDelay = 20;
-    digitalWrite(LED_PIN, HIGH);
-  }
- // String getTime = String(timer_count[3]) + "-" + String(timer_count[2]) + "-" + String(timer_count[1]) + "-" + String(timer_count[0]);
   
+  if (DynamicMode == true){
+  programDelay = 20;
+  digitalWrite(LED_PIN, HIGH);
+    while (dynamicTitle)
+    {
+    timeDIN = String(now.day()) + "." + String(now.month()) + "." + String(now.year()) + ";" + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()) ; 
+    appendFile(SD, "/DIN.csv", title.c_str());
+    appendFile(SD, "/DIN.csv", messageDIN.c_str());
+    dynamicTitle = !dynamicTitle;
+    }
+  dataMessage = String(result3, 6) + ";" + "\n";
+    if (millis()- last_time > programDelay){
+    last_time = millis();
+    appendFile(SD, "/DIN.csv", dataMessage.c_str());
+    }
+ // String getTime = String(timer_count[3]) + "-" + String(timer_count[2]) + "-" + String(timer_count[1]) + "-" + String(timer_count[0]);
+  }
   //dataMessage = String(result3, 6) + ";\n";
   //Check if button was pressed
 //  delay(1500);
@@ -167,10 +201,7 @@ unsigned long shiftIn(const int data_pin, const int clock_pin, const int bit_cou
 //   display.setCursor(0, 8);
 //   display.print(displayValue);
 
-// }
-// Append_inf to a file 
 void appendFile(fs::FS &fs, const char * path, const char * message){
-  Serial.printf("Appending to file: %s\n", path);
   File file = fs.open(path, FILE_APPEND);
   file.print(message);
   file.close();
@@ -184,8 +215,3 @@ void createDir(fs::FS &fs, const char * path){
     Serial.println("mkdir failed");
   }
 }
-
-
-
-
-
